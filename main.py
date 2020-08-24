6c3f239d76133f02e77cad190ad2c7db73861dad
Gavin Hunter <clghunter2@glow.sch.uk>
2020-08-24 18:11:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+Turtle Graphics
+
+This is a picture of *****, created with Python Turtle
+
+https://docs.python.org/3/library/turtle.html
+'''
+
+# load the turtle module so that we can use it in our program
+import turtle
+
+# create a turtle object called timmy
+# this is a copy of the turtle module with all the data and functions we need
+timmy = turtle.Turtle()
+
+# for timmy, run the forward() function and send 100 as the argument
+timmy.forward(100)
+
+'''
+You can delete lines 16 & 17 if you want. You picture should be coded below.
+'''
